9acebbff93887ed7b1351acaad7d9fef31f02aa3
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-06-03 12:25:42 +0200

    Added generation of generic header line for output

--- Tools/nitro_to_worker.py
+++ Tools/nitro_to_worker.py
@@ -19,11 +19,22 @@ def extract_cmd(line):
         sys.exit(1)
 
 
+def create_header(params_str):
+    nr_params = params_str.count(sep) + 1
+    param_names = ['param{0:d}'.format(i) for i in xrange(1, nr_params + 1)]
+    return sep.join(param_names)
+
+
 if __name__ == '__main__':
     extract_regex_str = r'cmd\s*=\s*\S+\s+(.+)$'
     fmt_regex_str = r'\s+'
     extract_regex = re.compile(extract_regex_str)
     fmt_regex = re.compile(fmt_regex_str)
+    line = sys.stdin.readline()
+    cmd = extract_cmd(line)
+    header = create_header(cmd)
+    print header
+    print cmd
     for line in sys.stdin:
         cmd = extract_cmd(line)
         print cmd
